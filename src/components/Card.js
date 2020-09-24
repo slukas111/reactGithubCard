3fbc6816f11049189ba6c9b0101884187fb6600f
Sasha Lukas <sashamati@gmail.com>
2020-09-24 11:00:45 -0700

    Got the project card ready. default image. Application is functioning as expected

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+
+class Card extends Component {
+    render() {
+        return (
+            <div className="github-profile">
+                <img src="/images/Github.PNG" alt="Github-card" />
+                <div className="info">
+                    <div className="name">Name here..</div>
+                </div>
+
+            </div>
+        );
+
+    }
+}
+
+export default Card;
